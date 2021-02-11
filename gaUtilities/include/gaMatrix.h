60120ca893870933378500cb638e3fa04e2b7eda
Ramses Guerrero A <42983204+RamsesGA@users.noreply.github.com>
2021-02-10 23:32:47 -0600

    //Nuevos cambios 10/02/21
    +Se cambió la estructura de la clase math
    +Se añadieron sus sobrecarga de operadores y una función de transpose
    -Falta añadir mas cosas

---
+++ gaUtilities/include/gaMatrix.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <vector>
+
+#include "gaPlatformTypes.h"
+
+using gaEngineSDK::int32;
+
+class Matrix
+{
+  public:
+    /*************************************************************************/
+    /**
+    * Constructor and Destructor
+    */
+    /*************************************************************************/
+
+    /**
+    * @brief Take the column size, row size and an initial value for each cell.
+    *        This will let you create any matrix.
+    */
+    Matrix(int32 columnSize = 4, int32 rowSize = 4, float value = 0.0f);
+
+    /**
+    * @brief Copy the members of one class instance to another.
+    */
+    Matrix(const Matrix& matrix);
+
+    ~Matrix() = default;
+
+    /*************************************************************************/
+    /**
+    * Normal methods.
+    */
+    /*************************************************************************/
+
+    /**
+    * @brief Let us convert our row in column and column in row.
+    */
+    Matrix
+    transpose();
+
+    /*************************************************************************/
+    /**
+    * Operators overloads.
+    */
+    /*************************************************************************/
+
+    /**
+    * @brief Let us add the data of one matrix with another one.
+    */
+    Matrix 
+    operator+(Matrix& matrix);
+
+    /**
+    * @brief Let us substract the data of one matrix with another one.
+    */
+    Matrix 
+    operator-(Matrix& matrix);
+
+    /**
+    * @brief Let us multiply the data of one matrix with another one.
+    */
+    Matrix 
+    operator*(Matrix& matrix);
+
+    /**
+    * @brief Let us add the data of one matrix with one data.
+    */
+    Matrix 
+    operator+(float data);
+
+    /**
+    * @brief Let us substract the data of one matrix with one data.
+    */
+    Matrix 
+    operator-(float data);
+
+    /**
+    * @brief Let us multiply the data of one matrix with one data.
+    */
+    Matrix 
+    operator*(float data);
+
+    /**
+    * @brief Let us divide the data of one matrix with one data.
+    */
+    Matrix 
+    operator/(float data);
+
+    /**
+    * @brief You can access to a specific position.
+    */
+    float& 
+    operator()(const int32& row, const int32& column);
+
+    /*************************************************************************/
+    /**
+    * Gets.
+    */
+    /*************************************************************************/
+
+    /**
+    * @brief Get the value of the row.
+    */
+    int32 
+    getRows() const;
+
+    /**
+    * @brief Get the value of the column.
+    */
+    int32 
+    getColumns() const;
+    
+
+  private:
+    int32 m_rowSize;
+    int32 m_columnSize;
+    std::vector<std::vector<float> > m_matrix;
+};
---
+++ gaUtilities/src/gaMatrix.cpp
@@ -0,0 +1,157 @@
+
+#include "gaMatrix.h"
+
+Matrix::Matrix(int32 columnSize, 
+                     int32 rowSize, 
+                     float value){
+  m_columnSize = columnSize;
+  m_rowSize = rowSize;
+  m_matrix.resize(rowSize);
+
+  for (int32 i = 0; i < m_matrix.size(); ++i){
+    m_matrix[i].resize(columnSize, value);
+  }
+}
+
+Matrix::Matrix(const Matrix& matrix){
+  this->m_columnSize = matrix.getColumns();
+  this->m_rowSize = matrix.getRows();
+  this->m_matrix = matrix.m_matrix;
+}
+
+/*************************************************************************/
+/**
+* Normal methods.
+*/
+/*************************************************************************/
+
+Matrix Matrix::transpose(){
+  Matrix trans(m_columnSize, m_rowSize, 0.0f);
+
+  for (int32 i = 0; i < m_columnSize; ++i){
+    for (int32 j = 0; j < m_rowSize; ++j) {
+      trans(i, j) = this->m_matrix[j][i];
+    }
+  }
+
+  return trans;
+}
+
+/*************************************************************************/
+/**
+* Operators overloads.
+*/
+/*************************************************************************/
+
+Matrix Matrix::operator+(Matrix& matrix){
+  Matrix add(m_columnSize, m_rowSize, 0.0f);
+
+  for (int32 i = 0; i < m_rowSize; ++i){
+    for (int32 j = 0; j < m_columnSize; ++j){
+      add(i, j) = this->m_matrix[i][j] + matrix(i, j);
+    }
+  }
+  return add;
+}
+
+Matrix Matrix::operator-(Matrix& matrix){
+  Matrix subs(m_columnSize, m_rowSize, 0.0);
+
+  for (int32 i = 0; i < m_rowSize; ++i){
+    for (int32 j = 0; j < m_columnSize; ++j){
+      subs(i, j) = this->m_matrix[i][j] - matrix(i, j);
+    }
+  }
+
+  return subs;
+}
+
+Matrix Matrix::operator*(Matrix& matrix){
+  Matrix multip(m_rowSize, matrix.getColumns(), 0.0f);
+
+  if (m_columnSize != matrix.getRows()){
+    //return "Error";
+  }
+
+  float temp = 0.0f;
+
+  for (int32 i = 0; i < m_rowSize; ++i) {
+    for (int32 j = 0; j < matrix.getColumns(); ++j) {
+      temp = 0.0f;
+
+      for (int32 k = 0; k < m_columnSize; ++k) {
+        temp += m_matrix[i][k] * matrix(k, j);
+      }
+
+      multip(i, j) = temp;
+    }
+  }
+
+  return multip;
+}
+
+Matrix Matrix::operator+(float data){
+  Matrix result(m_rowSize, m_columnSize, 0.0f);
+
+  for (int32 i = 0; i < m_rowSize; ++i){
+    for (int32 j = 0; j < m_columnSize; ++j){
+      result(i, j) = this->m_matrix[i][j] + data;
+    }
+  }
+
+  return result;
+}
+
+Matrix Matrix::operator-(float data){
+  Matrix result(m_rowSize, m_columnSize, 0.0f);
+
+  for (int32 i = 0; i < m_rowSize; ++i){
+    for (int32 j = 0; j < m_columnSize; ++j){
+      result(i, j) = this->m_matrix[i][j] - data;
+    }
+  }
+
+  return result;
+}
+
+Matrix Matrix::operator*(float data){
+  Matrix result(m_rowSize, m_columnSize, 0.0f);
+
+  for (int32 i = 0; i < m_rowSize; ++i){
+    for (int32 j = 0; j < m_columnSize; ++j){
+      result(i, j) = this->m_matrix[i][j] * data;
+    }
+  }
+
+  return result;
+}
+
+Matrix Matrix::operator/(float data){
+  Matrix result(m_rowSize, m_columnSize, 0.0);
+
+  for (int32 i = 0; i < m_rowSize; ++i){
+    for (int32 j = 0; j < m_columnSize; ++j){
+      result(i, j) = this->m_matrix[i][j] / data;
+    }
+  }
+
+  return result;
+}
+
+float& Matrix::operator()(const int32& row, const int32& column){
+  return this->m_matrix[row][column];
+}
+
+/*************************************************************************/
+/**
+* Gets.
+*/
+/*************************************************************************/
+
+int32 Matrix::getRows() const{
+  return this->m_rowSize;
+}
+
+int32 Matrix::getColumns() const{
+  return this->m_columnSize;
+}
