e0e8ef273b144b643f6cc379ab5568ecc837b44a
Ramses Guerrero A <42983204+RamsesGA@users.noreply.github.com>
2021-04-10 05:07:49 -0500

    //Cambios 10/04/21
    +Se eliminaron CPPS inservibles.
    +Se está implementando el funcionamiento de base app (se va a corregir y generar un hijo que herede de base app).

--- gaCore/include/gaBaseApp.h
+++ gaCore/include/gaBaseApp.h
@@ -1,15 +1,55 @@
 #pragma once
 
+#include <tchar.h>
 #include <SFML/Window.hpp>
 #include <gaVector2I.h>
+#include <gaMatrix4x4.h>
+#include <gaVector4.h>
 
 #include "gaPrerequisitesCore.h"
+#include "gaGraphicsApi.h"
+#include "gaCamera.h"
+#include "gaModels.h"
 
 using sf::Window;
 using sf::VideoMode;
 using sf::WindowHandle;
 
 namespace gaEngineSDK {
+  /***************************************************************************/
+  /**
+  * Structs
+  */
+  /***************************************************************************/
+  namespace ConstantBuffer1 {
+    struct E {
+      Matrix4x4 mView;
+      Matrix4x4 mProjection;
+    };
+  }
+
+  namespace ConstantBuffer2 {
+    struct E {
+      Matrix4x4 mWorld;
+      Vector4 vMeshColor;
+    };
+  }
+
+  namespace Matrices {
+    struct E{
+      Matrix4x4 World;
+      Matrix4x4 Projection;
+      Vector4   Color;
+    };
+  }
+
+  namespace ViewCB {
+    struct E{
+      Matrix4x4 View;
+    };
+  }
+  
+
 
   class GA_CORE_EXPORT BaseApp
   {
@@ -33,19 +73,19 @@ namespace gaEngineSDK {
       onInit() {};
 
       virtual void
-      update(float deltaTime) {};
+      update(float deltaTime);
 
       virtual void
       onUpdate(float deltaTime) {};
 
       virtual void
-      render() {};
+      render();
 
       virtual void
       onRender() {};
 
       virtual void
-      create() {};
+      create();
 
       virtual void
       onCreate() {};
@@ -54,13 +94,46 @@ namespace gaEngineSDK {
       resize(int32 width, int32 height) {};
 
       virtual void
-      destroySystem() {};
+      destroySystem();
+
+      virtual void
+      initCamera(bool isOGL = false);
+
+    protected:
+
+      //Tamaño de pantalla
+      const unsigned int m_width = 1000;
+      const unsigned int m_height = 800;
 
       Vector2I m_windowSize;
-
-      std::string m_windowTitle;
-
       Vector2I m_windowPosition;
+
+      Vector4 m_vMeshColor = (0.7f, 0.7f, 0.7f, 1.0f);
+
+      String m_windowTitle;
+
+      Matrix4x4 m_world;
+
+      Camera m_mainCamera;
+      Model* m_model = nullptr;
+
+      //GraphicsApi* g_pGraphicApi = new GraphicApiDX();
+      //GraphicsApi* g_pGraphicApi = new GraphicApiOGL();
+      GraphicsApi* m_pGraphicApi;
+
+      Textures* m_pRenderTargetView = nullptr;
+      Textures* m_pDepthStencil = nullptr;
+
+      InputLayout* m_pVertexLayout = nullptr;
+
+      Shaders* m_pBothShaders = nullptr;
+
+      VertexBuffer* m_pVertexBuffer = nullptr;
+
+      IndexBuffer* m_pIndexBuffer = nullptr;
+
+      ConstantBuffer* m_pConstantBuffer1 = nullptr;
+      ConstantBuffer* m_pConstantBuffer2 = nullptr;
 
       Window m_sfmlWindow;
   };
